a1e95310dcbee2aac7505d780186d5d98724e925
jianyingfeng <1055193533@qq.com>
2022-04-06 16:38:49 +0800

    11

--- test_fun1.py
+++ test_fun1.py
@@ -8,4 +8,4 @@ base_url = os.environ['base_url']
 
 def test_001():
     r = requests.get(url=base_url+'/testcases/5/')
-    assert r.json('testcase_name') == '查看项目列表接口_正向用例'
+    assert r.json()['testcase_name'] == '查看项目列表接口_正向用例'
